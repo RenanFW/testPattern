722a33697f46f1e3a073de7a741e64cf7567837b
RenanFW <renanwisniewski@gmail.com>
2025-03-07 15:37:38 +0000

    new page

---
+++ pages/matt/index.js
@@ -0,0 +1,5 @@
+const browser = document.querySelector(".brower");
+const browserVerson = document.querySelector(".browser-version")
+const os = document.querySelector(".os")
+const product = document.querySelector(".product")
+const heading = document.querySelector("h2.instruction")
