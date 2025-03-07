5a936f3642a329b3f23791f81a0b46d4640acecf
RenanFW <renanwisniewski@gmail.com>
2025-03-07 15:46:34 +0000

    matt page

--- pages/matt/index.js
+++ pages/matt/index.js
@@ -1,3 +1,6 @@
+
+import doc from "https://cdnjs.cloudflare.com/ajax/libs/platform/1.3.6/platform.min.js"
+
 const browser = document.querySelector(".brower");
 const browserVerson = document.querySelector(".browser-version")
 const os = document.querySelector(".os")
