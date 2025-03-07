df091861a4230f22ec8d09ab874960ed302c0afa
RenanFW <renanwisniewski@gmail.com>
2025-03-07 17:28:00 +0000

    a

--- pages/matt/index.js
+++ pages/matt/index.js
@@ -1,8 +1,1 @@
 
-import doc from 'https://cdnjs.cloudflare.com/ajax/libs/platform/1.3.6/platform.min.js';
-
-const browser = document.querySelector(".brower");
-const browserVerson = document.querySelector(".browser-version")
-const os = document.querySelector(".os")
-const product = document.querySelector(".product")
-const heading = document.querySelector("h2.instruction")
